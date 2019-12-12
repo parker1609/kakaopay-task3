36160399f043391183eb0b4fc02e389299802209
CODEMCD <psbum1609@gmail.com>
2019-12-12 12:25:06 +0900

    init project

---
+++ src/main/java/com/parksungbum/kakaopaytask3/KakaopayTask3Application.java
@@ -0,0 +1,13 @@
+package com.parksungbum.kakaopaytask3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaopayTask3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KakaopayTask3Application.class, args);
+    }
+
+}
