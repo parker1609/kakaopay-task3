ecbccb33e8721c080e07c7b8bb9738ca65a3b179
CODEMCD <psbum1609@gmail.com>
2019-12-14 16:56:57 +0900

    feat: IntegerConverter 구현
    * 숫자 문자열이 입력으로 주어졌을 때, 정상적인 Integer 자료형의 데이터로 변환해주는 함수

---
+++ src/main/java/com/parksungbum/kakaopaytask3/support/parser/IntegerConverter.java
@@ -0,0 +1,14 @@
+package com.parksungbum.kakaopaytask3.support.parser;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class IntegerConverter {
+
+    public int convert(String data) {
+        Pattern pattern = Pattern.compile("\\D");
+        Matcher matcher = pattern.matcher(data);
+        data = matcher.replaceAll("");
+        return Integer.parseInt(data);
+    }
+}
---
+++ src/test/java/com/parksungbum/kakaopaytask3/support/IntegerConverterTest.java
@@ -0,0 +1,36 @@
+package com.parksungbum.kakaopaytask3.support;
+
+import com.parksungbum.kakaopaytask3.support.parser.IntegerConverter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class IntegerConverterTest {
+
+    private IntegerConverter integerConverter = new IntegerConverter();
+
+    @Test
+    @DisplayName("숫자가 아닌 문자가 앞에 있는 경우 숫자로 변경한다.")
+    void convert_integer_prefix() {
+        String data = "\"10";
+
+        assertThat(integerConverter.convert(data)).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("숫자가 아닌 문자가 뒤에 있는 경우 숫자로 변경한다.")
+    void convert_integer_suffix() {
+        String data = "10\"";
+
+        assertThat(integerConverter.convert(data)).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("숫자가 아닌 문자가 여러 곳에 있는 경우 숫자로 변경한다.")
+    void convert_integer() {
+        String data = "\'1,000\"";
+
+        assertThat(integerConverter.convert(data)).isEqualTo(1000);
+    }
+}
