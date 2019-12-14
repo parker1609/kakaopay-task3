fa95df9943024f279670cfd19ee635fcc35716c2
CODEMCD <psbum1609@gmail.com>
2019-12-14 17:08:38 +0900

    chore: IntegerConverter Bean 등록

--- src/main/java/com/parksungbum/kakaopaytask3/support/config/WebConfiguration.java
+++ src/main/java/com/parksungbum/kakaopaytask3/support/config/WebConfiguration.java
@@ -1,6 +1,7 @@
 package com.parksungbum.kakaopaytask3.support.config;
 
 import com.parksungbum.kakaopaytask3.support.parser.CsvFileParser;
+import com.parksungbum.kakaopaytask3.support.parser.IntegerConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +11,10 @@ public class WebConfiguration {
     @Bean
     public CsvFileParser fileParserConfig() {
         return new CsvFileParser();
+    }
+
+    @Bean
+    public IntegerConverter integerConverterConfig() {
+        return new IntegerConverter();
     }
 }
